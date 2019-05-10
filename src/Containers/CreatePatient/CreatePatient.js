38fc24bdc92e96bfecf8f1d4e272edaacec10e97
Jack Laird <jack.laird0@gmail.com>
2019-05-10 14:02:17 -0600

    Add control form methods to  input form components

---
+++ src/Containers/CreatePatient/CreatePatient.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class CreatePatient extends Component {
+  state = {};
+
+  saveToState = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmitForm = event => {
+    event.preventDefault();
+    this.setState({
+    
+    }); 
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmitForm}>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CreatePatient;
