c56f6b53487726c50c28599c08b270e54af0e276
Tory Dannenberg <rfdberg@gmail.com>
2019-05-11 18:38:04 -0600

    Create components folder and Calendar component

---
+++ src/components/Calendar/Calendar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Calendar = props => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+Calendar.propTypes = {
+  
+};
+
+export default Calendar;
