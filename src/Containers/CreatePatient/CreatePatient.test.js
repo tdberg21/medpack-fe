f04e76ae71dccd3424d322254c5106c1aa19678b
Tory Dannenberg <rfdberg@gmail.com>
2019-05-11 18:38:53 -0600

    Add basic tests

--- src/Containers/CreatePatient/CreatePatient.test.js
+++ src/Containers/CreatePatient/CreatePatient.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ScreenResultsForm from './ScreenResultsForm';
+import CreatePatient from './CreatePatient';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<ScreenResultsForm />, div);
+  ReactDOM.render(<CreatePatient />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
