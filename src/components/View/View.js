2d0b88d644d0c9badf553160798ad23eee2c2433
Jack Laird <jack.laird0@gmail.com>
2019-05-13 16:16:57 -0600

    Add leftdrawer to view

--- src/components/View/View.js
+++ src/components/View/View.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import AppHeader from '../AppHeader/AppHeader';
+import LeftDrawer from '../LeftDrawer/LeftDrawer';
 
 const View = () => {
   return (
     <div className="root">
       <AppHeader />
+      <div>
+        <LeftDrawer />
+      </div>
     </div>
   );
 };
