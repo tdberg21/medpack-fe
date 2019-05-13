3f863ecad1822f01e0e8cc9971e0cc02db6a3943
Jack Laird <jack.laird0@gmail.com>
2019-05-13 14:34:20 -0600

    Create view component for all /app components

---
+++ src/components/View/View.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import AppHeader from '../AppHeader/AppHeader';
+
+const View = () => {
+  return (
+    <div className="root">
+      <AppHeader />
+    </div>
+  );
+};
+
+export default View;
