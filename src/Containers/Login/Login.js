5d635a2e20daa206f9e31fb48d81bbc037efdb55
Jack Laird <jack.laird0@gmail.com>
2019-05-13 17:59:53 -0600

    Add proptypes and lint

--- src/Containers/Login/Login.js
+++ src/Containers/Login/Login.js
@@ -1,45 +1,45 @@
 import React, { useState } from 'react';
 import { NavLink, withRouter } from 'react-router-dom';
-import { 
+import {
   Button,
   Divider,
   Paper,
   TextField,
   Typography,
-  withStyles
+  withStyles,
 } from '@material-ui/core';
 import { compose } from 'recompose';
 import ArrowIcon from '@material-ui/icons/ChevronRight';
+import PropTypes from 'prop-types';
 
-const styles = (theme) => ({
+const styles = theme => ({
   root: {
     width: 400,
     margin: '100px auto',
-    padding: theme.spacing.unit * 3
+    padding: theme.spacing.unit * 3,
   },
   button: {
     margin: `${theme.spacing.unit * 2}px 0`,
     display: 'flex',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   buttonText: {
     color: theme.palette.primary.contrastText,
     letterSpacing: 0.5,
-    fontWeight: 700
+    fontWeight: 700,
   },
   form: {
-    display: "flex",
-    flexDirection: 'column'
+    display: 'flex',
+    flexDirection: 'column',
   },
   navLink: {
-    margin: `${theme.spacing.unit}px 0`
+    margin: `${theme.spacing.unit}px 0`,
   },
-  textField: {
-  },
+  textField: {},
   title: {
-    margin: '5px 0'
-  }
-})
+    margin: '5px 0',
+  },
+});
 
 const Login = ({ classes }) => {
   const [email, setEmail] = useState('');
@@ -50,53 +50,59 @@ const Login = ({ classes }) => {
     // Send to back end for authentication...
     setEmail('');
     setPassword('');
-  }
+  };
 
   return (
     <Paper className={classes.root}>
-      <Typography variant="h5" className={classes.title}>Login</Typography>
+      <Typography variant="h5" className={classes.title}>
+        Login
+      </Typography>
       <form className={classes.form} onSubmit={handleSubmitForm}>
         <TextField
-          type="email" 
+          type="email"
           value={email}
           onChange={e => setEmail(e.target.value)}
-          variant='outlined'
-          label='Email'
+          variant="outlined"
+          label="Email"
           className={classes.textField}
-          margin='normal'
-          color='secondary'
+          margin="normal"
+          color="secondary"
         />
         <TextField
           type="password"
           name="password"
-          variant='outlined'
-          label='Password'
+          variant="outlined"
+          label="Password"
           value={password}
           onChange={e => setPassword(e.target.value)}
-          margin='normal'
-          color='secondary'
+          margin="normal"
+          color="secondary"
         />
-        <Button 
-          type="submit" 
-          variant='contained' 
+        <Button
+          type="submit"
+          variant="contained"
           className={classes.button}
-          color='primary'
+          color="primary"
         >
-          <Typography className={classes.buttonText} variant="subtitle1">Login</Typography>
-          <ArrowIcon/>
+          <Typography className={classes.buttonText} variant="subtitle1">
+            Login
+          </Typography>
+          <ArrowIcon />
         </Button>
       </form>
-      <Divider/>
-      <NavLink to='/SignUp'>
-        <Button className={classes.navLink}>
-          or Sign Up
-        </Button>
+      <Divider />
+      <NavLink to="/SignUp">
+        <Button className={classes.navLink}>or Sign Up</Button>
       </NavLink>
     </Paper>
   );
-}
+};
+
+Login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 export default compose(
   withRouter,
   withStyles(styles)
-)(Login)
+)(Login);
