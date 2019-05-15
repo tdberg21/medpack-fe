1199d1f3fc7fa1d0709e3a0469774bc959448ffc
Jack Laird <jack.laird0@gmail.com>
2019-05-15 15:17:50 -0600

    Add styling to view component

--- src/components/View/View.js
+++ src/components/View/View.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
+import { Route, withRouter, Switch, Redirect } from 'react-router-dom';
+import { withStyles } from '@material-ui/core';
+import { compose } from 'recompose';
 import AppHeader from '../AppHeader/AppHeader';
 import LeftDrawer from '../LeftDrawer/LeftDrawer';
+import Patients from '../Patients/Patients';
 
-const View = () => {
+const styles = theme => ({
+  root: {
+    display: 'flex',
+  },
+  content: {
+    boxSizing: 'border-box',
+    padding: theme.spacing.unit * 6,
+    width: '100%',
+    height: '100%',
+  },
+});
+
+const View = ({ classes }) => {
   return (
-    <div className="root">
+    <>
       <AppHeader />
-      <div>
+      <div className={classes.root}>
         <LeftDrawer />
+        <div className={classes.content}>
+          <Switch>
+            <Route to="/patients" component={Patients} />
+            <Redirect to="/app" />
+          </Switch>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
-export default View;
+export default compose(
+  withRouter,
+  withStyles(styles)
+)(View);
