37b7a8f5bee65585a423686a847aef1f67c7a69c
Jack Laird <jack.laird0@gmail.com>
2019-05-20 13:41:54 -0600

    Add export files for both components and container

---
+++ src/Containers/index.js
@@ -0,0 +1,6 @@
+import CreateAppointment from './CreateAppointment/CreateAppointment';
+import CreatePatient from './CreatePatient/CreatePatient';
+import Login from './Login/Login';
+import SignUp from './SignUp/SignUp';
+
+export { CreateAppointment, CreatePatient, Login, SignUp };
