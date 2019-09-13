1e82487db09296da4df438cc149bea0fd36b8fb3
Jack Laird <jack.laird0@gmail.com>
2019-09-13 11:08:39 -0600

    Add patients to state

--- src/util/Reducers/RootReducer/index.js
+++ src/util/Reducers/RootReducer/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import { handleUserLoginReducer } from '../userReducer/userReducer';
+import { handlePatientsReducer } from '../patientReducer/patientReducer';
 
 export const rootReducer = combineReducers({
   user: handleUserLoginReducer,
+  patients: handlePatientsReducer,
 });
