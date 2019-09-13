ae6d6eb7e1967c9d11096ba9dd2efcd557b82ce7
Jack Laird <jack.laird0@gmail.com>
2019-09-13 11:06:23 -0600

    Add patient details to index export file

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,17 @@
 import AppHeader from './AppHeader/AppHeader';
 import LeftDrawer from './LeftDrawer/LeftDrawer';
 import Patients from './Patients/Patients';
+import PatientDetails from './PatientDetails/PatientDetails';
 import UserInterface from './UserInterface/UserInterface';
 import View from './View/View';
 import Calendar from './Calendar/Calendar';
 
-export { AppHeader, LeftDrawer, Patients, UserInterface, View, Calendar };
+export {
+  AppHeader,
+  LeftDrawer,
+  Patients,
+  PatientDetails,
+  UserInterface,
+  View,
+  Calendar,
+};
