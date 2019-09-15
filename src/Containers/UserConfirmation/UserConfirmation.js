7313df4b8ae129da9282c9ec95176d833c003d45
Jack Laird <jack.laird0@gmail.com>
2019-09-15 12:16:14 -0600

    Add styling to userConfirmation

--- src/Containers/UserConfirmation/UserConfirmation.js
+++ src/Containers/UserConfirmation/UserConfirmation.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { confirmUser } from '../../util/ApiCalls';
 import { AppHeader } from '..';
 import { NavLink } from 'react-router-dom';
+import LogoImg from './../../Assets/logo.png';
 import {
   Button,
   Divider,
@@ -10,30 +11,70 @@ import {
   Typography,
   withStyles,
 } from '@material-ui/core';
+import { compose } from 'recompose';
 
+const styles = theme => ({
+  root: {
+    width: 800,
+    height: 600,
+    margin: '200px auto',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    margin: `${theme.spacing.unit * 3}px 0`,
+  },
+  img: {
+    width: 200,
+    height: 200,
+    borderRadius: 4,
+  },
+  button: {
+    width: 225,
+  },
+  buttonText: {
+    color: '#fff',
+  },
+});
 
 class UserConfirmation extends Component {
   render() {
-
-    const splitUrl = this.props.history.location.pathname.split('/user_confirmations/')[1];
+    const splitUrl = this.props.history.location.pathname.split(
+      '/user_confirmations/'
+    )[1];
     if (splitUrl) {
       console.log(splitUrl);
       const token = splitUrl.split('/confirm_email')[0];
       confirmUser(token);
       // history.push('/success');
-      console.log('success')
+      console.log('success');
     }
 
+    const { classes } = this.props;
+
     return (
-      <div>
-        <AppHeader />
-        <p>Thank you for confirming your account. Please contact an administrator to activate your account.</p>
+      <div className={classes.root}>
+        <img src={LogoImg} alt="logo" height="50" className={classes.img} />
+        <Typography variant="h5" className={classes.text}>
+          Thank you for confirming your account. <br /> Please contact an
+          administrator to activate your account.
+        </Typography>
         <NavLink to="/">
-          <Button>Click Here to Log In</Button>
+          <Button
+            variant="contained"
+            color="primary"
+            className={classes.button}
+          >
+            <Typography variant="subtitle1" className={classes.buttonText}>
+              Click Here to Log In
+            </Typography>
+          </Button>
         </NavLink>
       </div>
     );
   }
 }
 
-export default UserConfirmation;
+export default withStyles(styles)(UserConfirmation);
