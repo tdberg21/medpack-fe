163ed3dc1d7f0c986e53de11f1975b1ee86a244e
Jack Laird <jack.laird0@gmail.com>
2019-10-08 07:48:39 -0600

    Fix session  storage for user

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import eventCleaner from "../../util/Helpers/EventCleaner";
 import FullCalendar from "@fullcalendar/react";
 import { getAppointments } from "../../util/ApiCalls";
+import { userLogin } from "../../util/Actions";
 import { addEvents } from "../../util/Actions/events/events";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import dayGridPlugin from "@fullcalendar/daygrid";
@@ -17,11 +18,16 @@ class Calendar extends Component {
   }
 
   async componentDidMount() {
+    const user = await JSON.parse(sessionStorage.getItem("user"));
+    if (user) {
+      this.props.handleLogin(user);
+      console.log(user, "hello");
+    }
     const { office_id, auth_token } = this.props.userInfo;
     if (!this.props.events.length) {
       const response = await getAppointments(office_id, auth_token);
       const cleanedEvents = eventCleaner(response);
-      this.props.addEvents(cleanedEvents);
+      await this.props.addEvents(cleanedEvents);
     }
   }
 
@@ -45,7 +51,8 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  addEvents: events => dispatch(addEvents(events))
+  addEvents: events => dispatch(addEvents(events)),
+  handleLogin: user => dispatch(userLogin(user))
 });
 
 export default connect(
