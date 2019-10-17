21be7b04e9631af5adf08c9eda9598c641ed99ed
Jack Laird <jack.laird0@gmail.com>
2019-10-17 17:54:16 -0600

    Add update event action

--- src/util/Actions/events/events.js
+++ src/util/Actions/events/events.js
@@ -2,3 +2,9 @@ export const addEvents = events => ({
   type: "ADD_EVENTS",
   events
 });
+
+export const updateEvent = (id, updatedEvent) => ({
+  type: "UPDATE_EVENT",
+  id,
+  updatedEvent
+});
