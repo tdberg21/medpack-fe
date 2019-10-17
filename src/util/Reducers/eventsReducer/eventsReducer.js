451600ae4ca66d427e33e0130144af2d027b653d
Jack Laird <jack.laird0@gmail.com>
2019-10-17 17:54:47 -0600

    Add case for updating event

--- src/util/Reducers/eventsReducer/eventsReducer.js
+++ src/util/Reducers/eventsReducer/eventsReducer.js
@@ -3,6 +3,13 @@ export const eventsReducer = (state = [], action) => {
     case "ADD_EVENTS":
       const newState = [...state, ...action.events];
       return newState;
+    case "UPDATE_EVENT":
+      const targetedEventIndex = state.findIndex(
+        event => event.id === action.id
+      );
+      const stateCopy = [...state];
+      stateCopy[targetedEventIndex] = action.updatedEvent;
+      return stateCopy;
     default:
       return state;
   }
