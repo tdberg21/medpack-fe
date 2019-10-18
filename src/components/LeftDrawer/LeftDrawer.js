1ebf58bd04df3b9abcb8c9344b72f8ab235ed891
Jack Laird <jack.laird0@gmail.com>
2019-10-18 13:56:34 -0600

    Add link to admin dashbaord if user is admin

--- src/components/LeftDrawer/LeftDrawer.js
+++ src/components/LeftDrawer/LeftDrawer.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 import {
   Drawer,
   Divider,
@@ -7,84 +7,87 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
-  withStyles,
-} from '@material-ui/core';
-import HomeIcon from '@material-ui/icons/Home';
-import CalendarIcon from '@material-ui/icons/Event';
-import AppointmentIcon from '@material-ui/icons/Assignment';
-import PatientsIcon from '@material-ui/icons/AssignmentInd';
-import ScreeningIcon from '@material-ui/icons/Assessment';
-import MakeAppointmentIcon from '@material-ui/icons/SupervisedUserCircle';
-import MakePatientIcon from '@material-ui/icons/AccountCircle';
-import { NavLink } from 'react-router-dom';
+  withStyles
+} from "@material-ui/core";
+import HomeIcon from "@material-ui/icons/Home";
+import CalendarIcon from "@material-ui/icons/Event";
+import AppointmentIcon from "@material-ui/icons/Assignment";
+import PatientsIcon from "@material-ui/icons/AssignmentInd";
+import ScreeningIcon from "@material-ui/icons/Assessment";
+import MakeAppointmentIcon from "@material-ui/icons/SupervisedUserCircle";
+import MakePatientIcon from "@material-ui/icons/AccountCircle";
+import AdminIcon from "@material-ui/icons/VerifiedUser";
+import { connect } from "react-redux";
+import { compose } from "recompose";
+import { NavLink } from "react-router-dom";
 
 const drawerWidth = 240;
 
 const styles = theme => ({
   root: {
-    display: 'flex',
+    display: "flex"
   },
   active: {
     color: theme.palette.secondary.main,
-    backgroundColor: '#fdfdfd',
+    backgroundColor: "#fdfdfd"
   },
   appBar: theme.overrides.MuiAppBar.root,
   drawer: {
     width: drawerWidth,
-    flexShrink: 0,
+    flexShrink: 0
   },
   drawerPaper: {
-    width: drawerWidth,
+    width: drawerWidth
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 3
   },
   link: {
-    textDecoration: 'none',
-    color: 'initial',
+    textDecoration: "none",
+    color: "initial"
   },
   listItem: {
-    '&:hover': {
-      color: theme.palette.secondary.main,
-    },
-  },
+    "&:hover": {
+      color: theme.palette.secondary.main
+    }
+  }
 });
 
-const LeftDrawer = ({ classes }) => {
+const LeftDrawer = ({ classes, user }) => {
   const createNavOptions = () => {
     const navOptions = [
-      { text: 'Home', route: '', icon: <HomeIcon key="icon" /> },
+      { text: "Home", route: "", icon: <HomeIcon key="icon" /> },
       {
-        text: 'Office',
-        route: '/Office',
-        icon: <CalendarIcon key="icon" />,
+        text: "Office",
+        route: "/Office",
+        icon: <CalendarIcon key="icon" />
       },
       {
-        text: 'Appointments',
-        route: '/appointments',
-        icon: <AppointmentIcon key="icon" />,
+        text: "Appointments",
+        route: "/appointments",
+        icon: <AppointmentIcon key="icon" />
       },
       {
-        text: 'Patients',
-        route: '/patients',
-        icon: <PatientsIcon key="icon" />,
+        text: "Patients",
+        route: "/patients",
+        icon: <PatientsIcon key="icon" />
       },
       {
-        text: 'Screening Results',
-        route: '/screeningResults',
-        icon: <ScreeningIcon key="icon" />,
+        text: "Screening Results",
+        route: "/screeningResults",
+        icon: <ScreeningIcon key="icon" />
       },
       {
-        text: 'Add Appointment',
-        route: '/addAppointment',
-        icon: <MakeAppointmentIcon key="icon" />,
+        text: "Add Appointment",
+        route: "/addAppointment",
+        icon: <MakeAppointmentIcon key="icon" />
       },
       {
-        text: 'Add Patient',
-        route: '/addPatient',
-        icon: <MakePatientIcon key="icon" />,
-      },
+        text: "Add Patient",
+        route: "/addPatient",
+        icon: <MakePatientIcon key="icon" />
+      }
     ];
 
     return navOptions.map((option, index) => {
@@ -104,10 +107,29 @@ const LeftDrawer = ({ classes }) => {
               />
             </ListItem>
           </NavLink>
-          {index === 4 && <Divider />}
+          {index === navOptions.length - 3 && <Divider />}
         </>
       );
     });
+  };
+
+  const addAdminLink = () => {
+    const adminURL = `http://localhost:3001/admin?api_token=${user.auth_token}`;
+    return (
+      <>
+        <a className={classes.link} href={adminURL} target="_blank">
+          <ListItem button className={classes.listItem}>
+            <ListItemIcon>
+              <AdminIcon key="icon" />
+            </ListItemIcon>
+            <ListItemText
+              primary="Admin Dashboard"
+              className={classes.listItemText}
+            />
+          </ListItem>
+        </a>
+      </>
+    );
   };
 
   return (
@@ -115,17 +137,27 @@ const LeftDrawer = ({ classes }) => {
       className={classes.drawer}
       variant="permanent"
       classes={{
-        paper: classes.drawerPaper,
+        paper: classes.drawerPaper
       }}
     >
       <div className={classes.appBar} />
-      <List>{createNavOptions()}</List>
+      <List>
+        {createNavOptions()}
+        {user.role === "admin" && addAdminLink()}
+      </List>
     </Drawer>
   );
 };
 
+const mapStateToProps = state => ({
+  user: state.user
+});
+
 LeftDrawer.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(LeftDrawer);
+export default compose(
+  withStyles(styles),
+  connect(mapStateToProps)
+)(LeftDrawer);
